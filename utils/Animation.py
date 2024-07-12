c75a3120b4b71375c0ff4ed4b9ad1decdbe2fe23
Pete Pinter <pinter@p-squared.com>
2024-07-11 22:06:49 -0600

    Updated for numpy 2.0.0

--- utils/Animation.py
+++ utils/Animation.py
@@ -1,8 +1,5 @@
 import operator
-
 import numpy as np
-import numpy.core.umath_tests as ut
-
 import AnimationStructure
 from Quaternions_old import Quaternions
 
@@ -347,7 +344,7 @@ def transforms_multiply(t0s, t1s):
         together
     """
     
-    return ut.matrix_multiply(t0s, t1s)
+    return np.matmul(t0s, t1s)
     
 def transforms_inv(ts):
     fts = ts.reshape(-1, 4, 4)
